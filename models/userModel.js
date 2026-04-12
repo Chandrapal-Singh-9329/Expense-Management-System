0ee2b5c1e94c6e9f98009fbc7d00a7ce988d8033
Chandrapal <chandrapalsingh91711@gmail.com>
2026-04-12 17:48:57 +0530

    add user model, authentication routes and controllers

---
+++ models/userModel.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const userSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:[true, 'name is required']
+    },
+    email:{
+        type:String,
+        required:[true, 'email is required and should be unique'],
+        unique:true
+    },
+    password:{
+        type:String,
+        required:[true,'password is required']
+    }
+},
+{timeStamps:true})
+
+const userModel = mongoose.model('users', userSchema);
+
+export default userModel;
