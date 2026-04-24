f81d8c73a2258ce1173a325c5e61f77f0438dfb7
Chandrapal <chandrapalsingh91711@gmail.com>
2026-04-24 12:49:23 +0530

    Fix balance calculation and Add category wise income and expense analytics

--- client/src/components/Analytics.js
+++ client/src/components/Analytics.js
@@ -2,21 +2,51 @@ import React from 'react'
 import {Progress} from 'antd'
 
 const Analytics = ({allTransactions}) => {
+    const categories = [
+        'expense',
+        'food',
+        'movie',
+        'medicine',
+        'bill',
+        'fee',
+        'tip',
+        'project',
+        'tax'
+    ]
+
+
     const totalTransaction = allTransactions.length;
     const totalIncomeTransactions = allTransactions.filter(transaction => transaction.type === 'income');
     const totalExpenseTransactions = allTransactions.filter(transaction => transaction.type ==='expense');
     const totalIncomePercent = totalTransaction ? (totalIncomeTransactions.length / totalTransaction) * 100 : 0;
     const totalExpensePercent = totalTransaction ? (totalExpenseTransactions.length / totalTransaction) * 100 : 0 ;
 
-    // turnOver
-    const totalTurnover = allTransactions.reduce((acc, transaction) => acc + transaction.amount,0);
+   
+        // Total Income Amount
+    const totalIncome = totalIncomeTransactions.reduce(
+        (acc, transaction) => acc + transaction.amount,
+        0
+    );
 
-    const totalIncomeTurnover = totalIncomeTransactions.reduce((acc ,transaction)=> acc+  transaction.amount , 0);
-    
-    const totalExpenseTurnover = totalExpenseTransactions.reduce((acc ,transaction)=> acc+  transaction.amount , 0);
+    // Total Expense Amount
+    const totalExpense = totalExpenseTransactions.reduce(
+        (acc, transaction) => acc + transaction.amount,
+        0
+    );
 
-    const totalIncomeTurnoverPercent = (totalIncomeTurnover / totalTurnover) * 100;
-    const totalExpenseTurnoverPercent = (totalExpenseTurnover / totalTurnover) * 100;
+    // Remaining Balance
+    const totalBalance = totalIncome - totalExpense;
+
+    // Percentage based on money
+    const totalMoney = totalIncome + totalExpense;
+
+    const totalIncomePercentByAmount = totalMoney
+        ? (totalIncome / totalMoney) * 100
+        : 0;
+
+    const totalExpensePercentByAmount = totalMoney
+        ? (totalExpense / totalMoney) * 100
+        : 0;
     
 
   return (
@@ -41,24 +71,92 @@ const Analytics = ({allTransactions}) => {
             </div>
         </div>
 
-        <div className = 'col-md-4'>
-            <div className = 'card'>
-                <div className = 'card-header'>
-                    Total Turnover = {totalTurnover}
-                </div>  
-                    
-                <div className = 'card-body'>
-                    <h5 className='text-success'>Income Turnover : {totalIncomeTurnover} </h5>
-                    <h5 className='text-danger'>Expense Turnover : {totalExpenseTurnover} </h5>
-                    <div>
-                        <Progress type='circle' strokeColor='green' className='mx-2' percent= {totalIncomeTurnoverPercent.toFixed(0)} />
+        {/* Money Card */}
+                <div className='col-md-4'>
+                    <div className='card'>
 
-                        <Progress type='circle' strokeColor='red' className='mx-2' percent= {totalExpenseTurnoverPercent.toFixed(0)} />
+                        <div className='card-header'>
+                            Total Balance = ₹{totalBalance}
+                        </div>
+
+                        <div className='card-body'>
+
+                            <h5 className='text-success'>
+                                Total Income : ₹{totalIncome}
+                            </h5>
+
+                            <h5 className='text-danger'>
+                                Total Expense : ₹{totalExpense}
+                            </h5>
+
+                            <div>
+                                <Progress
+                                    type='circle'
+                                    strokeColor='green'
+                                    className='mx-2'
+                                    percent={totalIncomePercentByAmount.toFixed(0)}
+                                />
+
+                                <Progress
+                                    type='circle'
+                                    strokeColor='red'
+                                    className='mx-2'
+                                    percent={totalExpensePercentByAmount.toFixed(0)}
+                                />
+                            </div>
+
+                        </div>
 
                     </div>
                 </div>
-            </div>
+      </div>
+
+
+
+      {/* catrgory wise Income */}
+      <div className='row mt-3'>
+        <div className = 'col md-4'>
+            <h5 className="pt-4">Categorywise Income</h5>
+            {
+                categories.map((category)=>{
+                    const amount = allTransactions.filter((transaction) => transaction.type==='income' && transaction.category === category).reduce((acc, transaction)=> acc + transaction.amount , 0);
+
+                    return(
+                        amount > 0 &&
+                    <div className='card'>
+                        <div className='card-body'>
+                            <h5>{category}</h5>
+                            <Progress  percent = {((amount / totalIncome)*100).toFixed(0)}  />
+                            
+                        </div>
+                    </div>
+                    )
+                })
+            }
         </div>
+
+            {/* categorywise expense */}
+        <div className = 'col md-4'>
+            <h5 className="pt-4" >Categorywise Expense</h5>
+            {
+                categories.map((category)=>{
+                    const amount = allTransactions.filter((transaction) => transaction.type==='expense' && transaction.category === category).reduce((acc, transaction)=> acc + transaction.amount , 0);
+
+                    return(
+                        amount > 0 &&
+                    <div className='card'>
+                        <div className='card-body'>
+                            <h5>{category}</h5>
+                            <Progress  percent = {((amount / totalExpense)*100).toFixed(0)}  />
+                            
+                        </div>
+                    </div>
+                    )
+                })
+            }
+        </div>
+
+
       </div>
 
     </>
